b4aea88763a9e9155da64a4a29630929b374331b
Gustavo Costa <gcs5stars@gmail.com>
2023-06-01 23:50:31 -0300

    Bug fixes and performance improvements

--- Modules.GCScriptForPAD/ProcessText.cs
+++ Modules.GCScriptForPAD/ProcessText.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK;
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK.ActionSelectors;
 using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK.Attributes;
 using System;
+using System.ComponentModel;
 
 namespace Modules.GCScriptForPAD
 {
@@ -28,7 +30,7 @@ namespace Modules.GCScriptForPAD
                 teste = Tools.ProcessText(teste,
                                           true,
                                           ETextTrim.Trim,
-                                          ETextCase.ToLower,
+                                          ETextCase.ToUpper,
                                           ETextType.OnlyLettersNumbersSpaces,
                                           ETextRemoveSpaces.Duplicate);
             }
@@ -46,5 +48,106 @@ namespace Modules.GCScriptForPAD
         }
 
         #endregion
+    }
+
+    public enum ESelectorChoice
+    {
+        Selector1,
+        Selector2,
+        Selector3
+    }
+
+    [Action(Id = "CentralCustomAction")]
+    public class CentralCustomAction : ActionBase
+    {
+        #region Properties
+
+        [InputArgument, DefaultValue(ESelectorChoice.Selector1)]
+        public ESelectorChoice Selector { get; set; }
+
+        [InputArgument(Order = 1)]
+        public string FirstName { get; set; }
+
+        [InputArgument(Order = 2)]
+        public string LastName { get; set; }
+
+        [InputArgument(Order = 3)]
+        public int Age { get; set; }
+
+        [OutputArgument]
+        public string DisplayedMessage { get; set; }
+
+        #endregion
+
+        #region Methods Overrides
+
+        public override void Execute(ActionContext context)
+        {
+            if (Selector == ESelectorChoice.Selector1)
+            {
+                DisplayedMessage = $"Hello, {FirstName}!";
+            }
+            else if (Selector == ESelectorChoice.Selector2)
+            {
+                DisplayedMessage = $"Hello, {FirstName} {LastName}!";
+            }
+            else // The 3rd Selector was chosen 
+            {
+                DisplayedMessage = $"Hello, {FirstName} {LastName}!\nYour age is: {Age}";
+            }
+        }
+
+        #endregion
+    } // you can see below how to implement an action selector
+
+    [Action]
+    public class CentralCustomActionWithBoolean : ActionBase
+    {
+        #region Properties
+
+        [InputArgument, DefaultValue(true)]
+        public bool TimeExpired { get; set; }
+
+        [InputArgument]
+        public string ElapsedTime { get; set; }
+
+        [InputArgument]
+        public string RemainingTime { get; set; }
+
+        [OutputArgument]
+        public string DisplayedMessage { get; set; }
+
+        #endregion
+
+        #region Methods Overrides
+
+        public override void Execute(ActionContext context)
+        {
+            DisplayedMessage = TimeExpired ? $"The timer has expired. Elapsed time: {ElapsedTime}" : $"Remaining time: {RemainingTime}";
+        }
+
+        #endregion
+    }
+
+    public class NoTime : ActionSelector<CentralCustomActionWithBoolean>
+    {
+        public NoTime()
+        {
+            UseName("TimeHasExpired");
+            Prop(p => p.TimeExpired).ShouldBe(true);
+            ShowAll();
+            Hide(p => p.RemainingTime);
+        }
+    }
+
+    public class ThereIsTime : ActionSelector<CentralCustomActionWithBoolean>
+    {
+        public ThereIsTime()
+        {
+            UseName("TimeHasNotExpired");
+            Prop(p => p.TimeExpired).ShouldBe(false);
+            ShowAll();
+            Hide(p => p.RemainingTime);
+        }
     }
 }
---
+++ Modules.GCScriptForPAD/Selectors/CentralCustomActionSelectors.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK.ActionSelectors;
+
+namespace Modules.GCScriptForPAD.Selectors
+{
+    public class Selector1 : ActionSelector<CentralCustomAction>
+    {
+        public Selector1()
+        {
+            UseName("DisplayOnlyFirstName");
+            Prop(p => p.Selector).ShouldBe(ESelectorChoice.Selector1);
+            ShowAll();
+            Hide(p => p.LastName);
+            Hide(p => p.Age);
+            // or 
+            // Show(p => p.FirstName); 
+            // Show(p => p.DisplayedMessage);
+        }
+    }
+
+    public class Selector2 : ActionSelector<CentralCustomAction>
+    {
+        public Selector2()
+        {
+            UseName("DisplayFullName");
+            Prop(p => p.Selector).ShouldBe(ESelectorChoice.Selector2);
+            ShowAll();
+            Hide(p => p.Age);
+        }
+    }
+
+    public class Selector3 : ActionSelector<CentralCustomAction>
+    {
+        public Selector3()
+        {
+            UseName("DisplayFullDetails");
+            Prop(p => p.Selector).ShouldBe(ESelectorChoice.Selector3);
+            ShowAll();
+        }
+    }
+}
